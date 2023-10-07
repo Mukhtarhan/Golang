ae7af4bf6832d434400eeff6a23f59216a7def77
Mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-10-07 22:16:01 +0600

    helpers.go completed

--- assignment_2/cmd/api/helpers.go
+++ assignment_2/cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json" // New import
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,8 +9,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// Retrieve the "id" URL parameter from the current request context, then convert it to
-// an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -17,4 +16,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+// Define a writeJSON() helper for sending responses. This takes the destination
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
+// header map containing any additional HTTP headers we want to include in the response.
+type envelope map[string]interface{}
+
+// Change the data parameter to have the type envelope instead of interface{}.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
