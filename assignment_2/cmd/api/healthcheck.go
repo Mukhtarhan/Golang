81d26525de960dc14c7da8b1cda0119c76a9e0cc
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-12-01 13:28:41 +0600

    chapter 12

--- assignment_2/cmd/api/healthcheck.go
+++ assignment_2/cmd/api/healthcheck.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 )
 
@@ -17,28 +14,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	}
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		// Use the new serverErrorResponse() helper.
 		app.serverErrorResponse(w, r, err)
 	}
-}
-
-func (app *application) exampleHandler(w http.ResponseWriter, r *http.Request) {
-	var input struct {
-		Foo string `json:"foo"`
-	}
-	// Use io.ReadAll() to read the entire request body into a []byte slice.
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-	// Use the json.Unmarshal() function to decode the JSON in the []byte slice to the
-	// input struct. Again, notice that we are using a *pointer* to the input
-	// struct as the decode destination.
-	err = json.Unmarshal(body, &input)
-	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
-		return
-	}
-	fmt.Fprintf(w, "%+v\n", input)
 }
