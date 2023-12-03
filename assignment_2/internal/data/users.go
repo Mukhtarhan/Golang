1c00b2dd50eb6b4c8585d129be7b986509022ae3
mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2023-12-03 13:01:24 +0600

    chapter 16

--- assignment_2/internal/data/users.go
+++ assignment_2/internal/data/users.go
@@ -23,6 +23,8 @@ type UserModel struct {
 // using the json:"-" struct tag to prevent the Password and Version fields appearing in
 // any output when we encode it to JSON. Also notice that the Password field uses the
 // custom password type defined below.
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -31,6 +33,11 @@ type User struct {
 	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+// Check if a User instance is the AnonymousUser.
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 // Create a custom password type which is a struct containing the plaintext and hashed
